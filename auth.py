59ce0cfc05c6f7da84be79fbeb8c8423914b7d4e
Trinethr N <trinethr@Neels-MacBook-Air.local>
2024-11-06 01:37:21 +0530

    updated auth error messages

--- auth.py
+++ auth.py
@@ -9,17 +9,20 @@ auth = Blueprint('auth', __name__)
 def login():
     data = request.get_json()
 
-    if data.get('email') is ("" or None) or data.get('password') is ("" or None): 
-        return jsonify({"message": "Enter valid email and password", "authenticated": False})
-    
+    if data is None or len(data) == 0 or data.get('email') is None or data.get('password') is None:
+        return jsonify({"message": "Application error. Please try again later or <a class='errorcontact' href='https://github.com/soundshareapp/' target='_blank'>contact us.</a>", "authenticated": False})
+
+    if data.get('email') is "" or data.get('password') is "":
+        return jsonify({"message": "Enter a valid email and password to proceed.", "authenticated": False})
+
     user = User.get(data.get('email'))
-    
+
     if user is None:
-        return jsonify({"message": "User is not registered", "authenticated": False})
-    
-    if not user.check_password(data.get('password')):   
-        return jsonify({"message": "Incorrect password", "authenticated": False})
-    
+        return jsonify({"message": "No account found for this email. Please sign up.", "authenticated": False})
+
+    if not user.check_password(data.get('password')):
+        return jsonify({"message": "Email and password do not match.", "authenticated": False})
+
     login_user(user, remember=data.get("staySignedIn"))
     return jsonify({"message": "Successfully logged in!", "authenticated": True})
 
@@ -33,4 +36,3 @@ def logout():
 @auth.route('/status', methods=['GET'])
 def status():
     return jsonify({"authenticated": current_user.is_authenticated})
-    
