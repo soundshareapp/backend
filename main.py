908c8f97704784b302fee4f2c960d9aeb2457ad3
Trinethr <72494265+xXTgamerXx@users.noreply.github.com>
2024-11-06 17:21:09 +0530

    removed unncessary session

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from flask_session import Session 
 from flask_cors import CORS
 from auth import auth
 from flask_login import LoginManager
@@ -13,7 +12,6 @@ CORS(app, origins='http://localhost:5173', supports_credentials=True)
 
 app.config['SECRET_KEY'] = 'mysecret' # Change in production
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['SESSION_COOKIE_SAMESITE']='None'
 app.config['SESSION_COOKIE_SECURE']='True'
 
@@ -24,8 +22,6 @@ app.config['SESSION_SQLALCHEMY'] = db
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.init_app(app)
-
-sess = Session(app)
 
 app.register_blueprint(auth, url_prefix='/auth')
 
