77f937553454d85bc095a1beaf452cab94c682ed
Trinethr <72494265+xXTgamerXx@users.noreply.github.com>
2025-01-29 01:42:42 +0530

    added method to get multiple params at once

--- routes/userdata.py
+++ routes/userdata.py
@@ -23,14 +23,27 @@ def update_data():
  
     return jsonify({'updated': list(data.keys())})
  
+@userdata.route("/get_multiple/<params>", methods=["GET"])
+@login_required
+def get_multiple_data(params):
+    params_list = params.split(',')
+    data = UserData.get(current_user.id)
+    result = {}
+    for param in params_list:
+        value = getattr(data, param, None)
+        if value is None:
+            return jsonify({"message": f"Invalid parameter: {param}"}), 400
+        result[param] = value
+
+    return jsonify(result)
+
 @userdata.route("/<param>", methods=["GET"])
 @login_required
-def get_data(param):
-    print(param)
+def get_data(param:str):
     if (param != 'spotify_token' and param != 'spotify_refresh_token'):
         data = UserData.get(current_user.id)
         value = getattr(data, param)
         if value is None:
             return jsonify({"message": f"Invalid parameter: {param}"}), 400
- 
         return value
+    
